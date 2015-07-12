2067546ca0f7d8e600bce5714aeadc68de956e18
KZRKZR <KZRKZR@gmail.com>
2015-07-12 17:44:06 +0300

    Update reading NA

--- plot1.R
+++ plot1.R
@@ -1,8 +1,7 @@
 ## read data
-test<-read.table("household_power_consumption.txt",header=TRUE,sep=";",colClasses=c("character", "character"))
+test<-read.table("household_power_consumption.txt",header=TRUE,sep=";",colClasses=c("character", "character"),na.strings="?")
 ## only two dates
 need<-test[test$Date=="1/2/2007"|test$Date=="2/2/2007",]
-need[need=="?"]<-"NA"
 ## convert to numeric
 need[, c(3:9)] <- sapply(need[, c(3:9)], as.numeric)
 ## add datetime column
