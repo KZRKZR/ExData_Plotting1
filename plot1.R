36c4ac5a545549ddf667940f5a20aab4dbe2aba2
KZRKZR <KZRKZR@gmail.com>
2015-07-12 19:11:53 +0300

    Update reading data

--- plot1.R
+++ plot1.R
@@ -1,11 +1,13 @@
-## read data
-test<-read.table("household_power_consumption.txt",header=TRUE,sep=";",colClasses=c("character", "character"),na.strings="?")
+##assume file located at working directory 
+##read data
+test<-read.table("household_power_consumption.txt",header=TRUE,sep=";",colClasses=c("character", "character",rep("numeric",7)),na.strings="?")
+
 ## only two dates
 need<-test[test$Date=="1/2/2007"|test$Date=="2/2/2007",]
-## convert to numeric
-need[, c(3:9)] <- sapply(need[, c(3:9)], as.numeric)
+
 ## add datetime column
 need$DateTime <- as.POSIXct(paste(need$Date, need$Time), format="%d/%m/%Y %H:%M:%S")
+
 ##plot
 png(width=480, height=480,file = "plot1.png")
 hist(need$Global_active_power,col="red",main="Global Active Power",xlab="Global Active Power (killowatts)")
--- plot4.R
+++ plot4.R
@@ -1,14 +1,15 @@
 ## read data
-test<-read.table("household_power_consumption.txt",header=TRUE,sep=";",colClasses=c("character", "character"))
+test<-read.table("household_power_consumption.txt",header=TRUE,sep=";",colClasses=c("character", "character",rep("numeric",7)),na.strings="?")
+
 ## only two dates
 need<-test[test$Date=="1/2/2007"|test$Date=="2/2/2007",]
-need[need=="?"]<-"NA"
-## convert to numeric
-need[, c(3:9)] <- sapply(need[, c(3:9)], as.numeric)
+
 ## add datetime column
 need$DateTime <- as.POSIXct(paste(need$Date, need$Time), format="%d/%m/%Y %H:%M:%S")
+
 ##set locale
 Sys.setlocale("LC_ALL","C")
+
 ##plot
 png(width=480, height=480,file = "plot4.png")
 par(mfrow=c(2,2))
